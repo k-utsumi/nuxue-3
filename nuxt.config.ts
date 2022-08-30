dc38022609783484ef6a6a151a894c6f7c47d380
k-utsumi <kaba.box@gmail.com>
2022-08-30 15:46:56 +0900

    🔧 @tsconfig/strictest
    ref. https://tech.andpad.co.jp/entry/2022/06/29/090000
    
    - tsconfig.json 内のエラー回避に `strictNullChecks` 追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    tsConfig: {
+      extends: "@tsconfig/strictest/tsconfig.json",
+      compilerOptions: {
+        strictNullChecks: true,
+      },
+    },
+  },
+});
