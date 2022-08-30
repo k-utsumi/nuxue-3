bf5998ff1f3277cd52e282af10632bef205cf6d1
k-utsumi <kaba.box@gmail.com>
2022-08-30 18:33:50 +0900

    ⏪ Revert "🔧 @tsconfig/strictest"
    This reverts commit dc38022609783484ef6a6a151a894c6f7c47d380.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  typescript: {
-    tsConfig: {
-      extends: "@tsconfig/strictest/tsconfig.json",
-      compilerOptions: {
-        strictNullChecks: true,
-      },
-    },
-  },
-});
+
+})
